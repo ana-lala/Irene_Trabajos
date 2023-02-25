f9aec398be310ad831e660a6ddf586ba995b1cd6
ana-lala <58317934+ana-lala@users.noreply.github.com>
2023-02-25 08:49:01 -0700

    Create monio.cpp

---
+++ Figuritas/monio.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+#include <cstdlib>
+#include <limits>
+#define MAXN 21
+#define MINN 5
+
+int main()
+{
+      int n;
+  cout << "Programa que imprime una piramide de asteriscos impar" << endl << endl;
+
+ do{
+     if(cin.fail()){
+         cin.clear();
+         cin.ignore(numeric_limits<int>::max(), '\n');
+     }
+    cout << "N\243mero de lineas (Tama\244o M\240ximo= 29 M\241nimo = 4): ";
+    cin >> n;
+    }while (n < MINN || n > MAXN|| cin.fail()|| n%2==0 );
+
+    int mitad = (n/2)+1;
+    
+    
+     for(int i = 0; i<mitad; i++){
+         for(int j= 0; j<=i; j++){
+             cout << "*";
+         }
+         for(int j= mitad-1; j>i; j--){
+             cout << "  ";
+         }
+         for(int j= 0; j<=i; j++){
+             cout << "*";
+         }
+         cout << endl;
+     }
+     
+     for(int i = mitad-1; i>0; i--){
+         for(int j= 0; j<i; j++){
+             cout << "*";
+         }
+         for(int j= mitad; j>i; j--){
+             cout << "  ";
+         }
+         for(int j= 0; j<i; j++){
+             cout << "*";
+         }
+         cout << endl;
+     }
+   
+    return 0;
+}
