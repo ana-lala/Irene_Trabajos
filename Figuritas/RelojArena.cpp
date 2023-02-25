80aa363fc144ed69721e246677f2d586c6f013da
ana-lala <58317934+ana-lala@users.noreply.github.com>
2023-02-25 00:52:19 -0700

    Update RelojArena.cpp

--- Figuritas/RelojArena.cpp
+++ Figuritas/RelojArena.cpp
@@ -1,21 +1,28 @@
 #include <iostream>
 using namespace std;
 #include <cstdlib>
-#define MAXN 29
-#define MINN 2
+#include <limits>
+#define MAXN 21
+#define MINN 5
+
 int main()
 {
       int n;
-
-
   cout << "Programa que imprime una piramide de asteriscos impar" << endl << endl;
 
  do{
+     if(cin.fail()){
+         cin.clear();
+         cin.ignore(numeric_limits<int>::max(), '\n');
+     }
     cout << "N\243mero de lineas (Tama\244o M\240ximo= 29 M\241nimo = 4): ";
     cin >> n;
-    }while (n < MINN || n > MAXN );
+    }while (n < MINN || n > MAXN|| cin.fail()|| n%2==0 );
+
+    int mitad = (n/2)+1;
     
-      for(int i = n; i>1; i--) {
+    
+     for(int i = mitad; i>1; i--) {
          for(int j = n; j>i; j--){
              cout << " ";
          }
@@ -25,7 +32,7 @@ int main()
        cout << endl;
      }
     
-    for(int i = 1; i<=n; i++) {
+    for(int i = 1; i<=mitad; i++) {
          for(int j = n; j>i; j--){
              cout << " ";
          }
